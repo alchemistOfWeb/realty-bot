2c313e5630b0bcc598c07a6f55f1713a8920ef5c
Alchemist of the web <nikitakzntcv@gmail.com>
2024-12-01 13:57:38 +0400

    add entrypoint.sh update dockerfile.railway

---
+++ etrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Запуск миграций и бота
+poetry run python manage.py migrate
+poetry run python bot.py &  # Запускаем бота в фоновом режиме
+
+# Запуск Celery
+poetry run celery -A config worker -l info
